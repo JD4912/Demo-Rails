149966873f53ed372450bd2bf259fc20cdeb99ae
JD4912 <58825633+JD4912@users.noreply.github.com>
2022-04-29 12:19:45 +0530

    Update beforeInstall.sh

--- scripts/beforeInstall.sh
+++ scripts/beforeInstall.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/
 
 rm -rf /var/www/ruby
-git clone https://github.com/JD4912/ruby_test.git /var/www/ruby
+git clone https://github.com/JD4912/Demo-Rails.git /var/www/ruby
 # cd /var/www/ruby && rm Gemfile.lock 
 cd /var/www/ruby/ && rm Gemfile.lock
 #gem install bundler 
